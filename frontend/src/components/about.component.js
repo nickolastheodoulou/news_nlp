29828eab1b23b1cc3651cf28d494091f1ed32e77
colin <nicktheodoulou>
2020-06-16 14:29:59 +0100

    - set state based on json file, now need to convert to a get request

---
+++ frontend/src/components/about.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Created by Nick Theodoulou</h3>
+      </div>
+    )
+  }
+}
+
+export default About;
