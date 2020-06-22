6cdb9c7eba8f76f4b2b4928fcb521ac2e3aed110
colin <nicktheodoulou>
2020-06-22 18:20:37 +0100

    - added navbar styling, will next add a menu

--- frontend/src/components/navbar/navbar.component.js
+++ frontend/src/components/navbar/navbar.component.js
@@ -1,34 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default class Navbar extends Component {
 
 
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <div className="collpase navbar-collapse">
-          <ul className="navbar-nav mr-auto">
 
-            <li className="navbar-item">
-              <Link to="/" className="navbar-brand">Home</Link>
-            </li>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: '#FFFFFF',
+    textDecoration: 'none',
+  },
+  menuItem: {
+    color: '#FFFFFF',
+    textDecoration: 'none',
 
-            <li className="navbar-item">
-              <Link to="/finance" className="navbar-brand">Finance</Link>
-            </li>
 
-            <li className="navbar-item">
-              <Link to="/bitcoin" className="navbar-brand">Bitcoin</Link>
-            </li>
+}
+}));
 
-            <li className="navbar-item">
-              <Link to="/about" className="nav-link">About</Link>
-            </li>
+export default function Navbar() {
 
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/*
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          */}
+
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/" className={classes.title}>
+              Home
+            </Link>
+          </Typography>
+
+          <Button color="inherit">
+            <Link to="/finance" className={classes.menuItem}>
+              Finance
+            </Link>
+          </Button>
+
+
+
+          <Link to="/bitcoin" className={classes.menuItem}>
+            <Button color="inherit">
+              Bitcoin
+            </Button>
+          </Link>
+
+
+
+          <Link to="/about" className={classes.menuItem}>
+            <Button color="inherit">
+              about
+            </Button>
+          </Link>
+
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+
 }
