fe17646575c4100242f0fec8120ba949114c754b
colin <nicktheodoulou>
2020-06-26 09:31:04 +0100

    - restructured project for production

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -14,7 +15,8 @@ const today = new Date();
 const dateToday = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
 app.get('/getnews', (req, res) => {
-
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   request(`http://newsapi.org/v2/${req.query.newsType}?language=${req.query.language}&country=${req.query.country}&q=${req.query.q}&category=${req.query.category}&pageSize=10&page=${req.query.pageNumber}&from=${dateToday}&sortBy=publishedAt&apiKey=1c3ed04ce9914eebb242b09bd5fbf836`, function (error, response, body) {
     res.send(body)
 
