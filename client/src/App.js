d9b44ef454bfca19c24f330cf9aa1513b9ce182d
colin <nicktheodoulou>
2020-08-29 18:29:08 +0100

    - converted App to pull from articleData, need to convert Navbar to pull from articleData.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,96 +5,36 @@ import Navbar from "./components/navbar/navbar.component";
 import About from "./components/about/about.component";
 import News from './components/newslist/news.list.component'
 
+import articleData from "./data/articleData";
 
 
 function App() {
-  return (
 
+  return (
     <Router>
       <div className="container">
         <Navbar />
+        <div>
+          {
+            articleData.work.map((item) => {
+              return (
+                <Route exact path={item.path} render={(props) =>
+                  <News {...props}
+                        navbarTitle={item.navbarTitle}
+                        newsType={item.newsType}
+                        q={item.q}
+                        language={item.language}
+                        country={item.country}
+                        category={item.category}
+                        ticker={item.ticker}
+                        tickerSource={item.tickerSource}
+                        assetType={item.assetType}
+                  />}/>
+              )
+            })
+          }
+        </div>
 
-
-        <Route exact path="/search" render={(props) =>
-          <News {...props}
-                navbarTitle={"Your Search"}
-                newsType={"everything"}
-                q={"Apple"}
-                language={"en"}
-                country={""}
-                category={""}
-          />} />
-
-        <Route exact path="/" render={(props) =>
-          <News {...props}
-                navbarTitle={"US Global News"}
-                newsType={"top-headlines"}
-                language={"en"}
-                country={"us"}
-                q={""}
-                category={""}
-                ticker={""}
-          />} />
-
-        <Route exact path="/usbusiness" render={(props) =>
-          <News {...props}
-                navbarTitle={"US business News"}
-                newsType={"top-headlines"}
-                q={""}
-                language={"en"}
-                country={"us"}
-                category={"business"}
-                ticker={""}
-          />} />
-
-        <Route exact path="/bitcoin" render={(props) =>
-          <News {...props}
-                navbarTitle={"Bitcoin"}
-                newsType={"everything"}
-                q={"\"bitcoin\""}
-                language={"en"}
-                country={""}
-                category={""}
-                ticker={"BTCUSDT"}
-                assetType={'crypocurrency'}
-          />} />
-
-
-        <Route exact path="/ethereum" render={(props) =>
-          <News {...props}
-                navbarTitle={"Ethereum"}
-                newsType={"everything"}
-                q={"ethereum"}
-                language={"en"}
-                country={""}
-                category={""}
-                ticker={"ETHUSDT"}
-                assetType={'crypocurrency'}
-          />} />
-
-        <Route exact path="/litecoin" render={(props) =>
-          <News {...props}
-                navbarTitle={"Litecoin"}
-                newsType={"everything"}
-                q={"litecoin"}
-                language={"en"}
-                country={""}
-                category={""}
-                ticker={"LTCUSDT"}
-                assetType={'crypocurrency'}
-          />} />
-
-        <Route exact path="/apple" render={(props) =>
-          <News {...props}
-                navbarTitle={"Apple"}
-                newsType={"everything"}
-                q={"apple"}
-                language={"en"}
-                country={""}
-                category={""}
-                ticker={""}
-                assetType={'stock'}
-          />} />
 
         <Route path="/about" exact component={About} />
       </div>
---
+++ client/src/data/articleData.js
@@ -0,0 +1,94 @@
+import News from "../components/newslist/news.list.component";
+import React from "react";
+
+const articleData = {
+  "work":[
+    {
+      "path": "/search",
+      "navbarTitle": "Your Search",
+      "newsType": "everything",
+      "q": "Apple",
+      "language": "en",
+      "country": "",
+      "category": "",
+      "ticker":"",
+      "tickerSource":""
+    },
+    {
+      "path": "/",
+      "navbarTitle": "US Global News",
+      "newsType": "top-headlines",
+      "q": "",
+      "language": "en",
+      "country": "us",
+      "category": "",
+      "ticker":"",
+      "tickerSource":""
+    },
+    {
+      "path": "/usbusiness",
+      "navbarTitle": "US business News",
+      "newsType": "top-headlines",
+      "q": "",
+      "language": "en",
+      "country": "us",
+      "category": "business",
+      "ticker":"",
+      "tickerSource":""
+    },
+    {
+      "path": "/bitcoin",
+      "navbarTitle": "Bitcoin",
+      "newsType": "everything",
+      "q":"bitcoin",
+      "language": "en",
+      "country": "",
+      "category": "",
+      "ticker":"BTCUSDT",
+      "assetType":"crypocurrency",
+      "tickerSource":"Binance"
+    },
+    {
+      "path": "/ethereum",
+      "navbarTitle": "Ethereum",
+      "newsType": "everything",
+      "q": "ethereum",
+      "language": "en",
+      "country": "",
+      "category": "",
+      "ticker":"ETHUSDT",
+      "assetType":"crypocurrency",
+      "tickerSource":"Binance"
+    },
+    {
+      "path": "/litecoin",
+      "navbarTitle": "Litecoin",
+      "newsType": "everything",
+      "q": "litecoin",
+      "language": "en",
+      "country": "",
+      "category": "",
+      "ticker":"LTCUSDT",
+      "assetType":"crypocurrency",
+      "tickerSource":"Binance"
+    },
+    {
+      "path": "/apple",
+      "navbarTitle": "Apple",
+      "newsType": "everything",
+      "q": "apple",
+      "language": "en",
+      "country": "",
+      "category": "",
+      "ticker":"",
+      "assetType":"stock",
+      "tickerSource":"finnhub"
+    }
+  ]
+}
+
+export default articleData
+
+
+
+
